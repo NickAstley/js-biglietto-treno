1005e9f04037da3878f52db03c8b211211ad714b
NickAstley <andreanicastro1994@gmail.com>
2022-05-18 13:16:35 +0200

    Initializing repo

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const scontoMinorenni = 0.2;
+const scontoOver = 0.4;
+const km = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
+const eta = parseInt(prompt("Qual è la tua età?"));
+
+console.log(km);
